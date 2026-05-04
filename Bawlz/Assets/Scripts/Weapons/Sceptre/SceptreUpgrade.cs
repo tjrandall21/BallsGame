665d4e9e861d4ed508667e39ae7c304bf0711d49
tjrandall21 <tjrandall21@gmail.com>
2026-05-04 08:38:34 -0700

    sceptre upgrades + other fixes

---
+++ Bawlz/Assets/Scripts/Weapons/Sceptre/SceptreUpgrade.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceptreUpgrade", menuName = "Scriptable Objects/SceptreUpgrade")]
+public class SceptreUpgrade : WeaponUpgrade
+{
+    public float minionDamageScaling = 0;
+    public float minionDamage = 0;
+    public float minionHealth = 0;
+    public float minionHealthScaling = 0;
+    public int minionsPerHit = 0;
+}
