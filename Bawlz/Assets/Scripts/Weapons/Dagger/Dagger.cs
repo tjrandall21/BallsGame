d794c387717faa68e8693bc157177a468a885f51
tjrandall21 <tjrandall21@gmail.com>
2026-05-06 21:54:16 -0700

    added weapon sprites

--- Bawlz/Assets/Scripts/Weapons/Dagger/Dagger.cs
+++ Bawlz/Assets/Scripts/Weapons/Dagger/Dagger.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class Dagger : Weapon
 {
     [SerializeField] private DoTEffect poisonEffect;
+
+    public float durationScaling = 0.3f;
+    public float damageScaling = 0.3f;
+
+    protected override void Start()
+    {
+        base.Start();
+        poisonEffect = new DoTEffect{damagePerSecond = 1f,statusDuration = 2f,statusName = "Dagger Poison"};
+    }
     protected override void OnBallHit(BallController otherBall)
     {
         Debug.Log("Ball Collision");
@@ -17,7 +26,7 @@ public class Dagger : Weapon
         }
 
         otherBall.ApplyStatus(poisonEffect, parent); 
-
+        poisonEffect.damagePerSecond += damageScaling;
+        poisonEffect.statusDuration += durationScaling;
     }
-
 }
