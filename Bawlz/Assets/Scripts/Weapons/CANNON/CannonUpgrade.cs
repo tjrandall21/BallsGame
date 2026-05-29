3a799a7fd488c8f0ef947304d322d52ea14668e6
ArthurH-cmd <arthurheymans101@gmail.com>
2026-05-29 12:18:05 -0700

    Airstrike and FireRate

--- Bawlz/Assets/Scripts/Weapons/CANNON/CannonUpgrade.cs
+++ Bawlz/Assets/Scripts/Weapons/CANNON/CannonUpgrade.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "CannonUpgrade", menuName = "Weapon Upgrades/Cannon Upgrades/CannonUpgrade")]
 public class CannonUpgrade : WeaponUpgrade
 {
     [SerializeField] public int u_prodCount = 0;
     [SerializeField] public float u_projSpeed = 0;
     [SerializeField] public int u_projDamage = 0;
-
+    [SerializeField] public int attackSpeedScaling = 0;
     public override void OnAttack() { }
     public virtual void OnProjectileHit(BallController otherBall, Projectile projectile) { }
     public virtual void OnProjectileWeaponHit(Weapon otherWeapon, Projectile projectile) { }
---
+++ Bawlz/Assets/Scripts/Weapons/CANNON/CannonUpgrades/AirStrike.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AirStrike", menuName = "Weapon Upgrades/Cannon Upgrades/AirStrike")]
+public class AirStrike : CannonUpgrade
+{
+    [SerializeField] GameObject airStrikePrefab;
+    [SerializeField] float airStrikeDamage = 20f;
+    [SerializeField] float airStrikeKnockback = 30f;
+    [SerializeField] float airStrikeExplosionSize = 1f;
+    [SerializeField] float airStrikeDelay = 1f;
+    [SerializeField] float airStrikeSize = 1.2f;
+
+    public override void OnBallHit(BallController otherBall)
+    {
+        base.OnBallHit(otherBall);
+
+        GameObject airStrikeObject = Instantiate(airStrikePrefab);
+        airStrikeObject.layer = parentBall.gameObject.layer + 4;
+        CannonAirStrike airStrike = airStrikeObject.GetComponent<CannonAirStrike>();
+        airStrike.AirStrikeInit(airStrikeDamage, airStrikeKnockback, airStrikeExplosionSize, airStrikeDelay, otherBall.transform.position);
+        airStrike.Init(0.0f, otherBall.transform.position, airStrikeDelay, airStrikeSize);
+    }
+}
