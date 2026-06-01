82043eedb71839841b3121bc490b813f6b745f75
ArthurH-cmd <arthurheymans101@gmail.com>
2026-05-31 19:02:48 -0700

    Not perfect fix but works for now

--- Bawlz/Assets/Scripts/Weapons/Hammer/HammerUpgrades/LightningBall.cs
+++ Bawlz/Assets/Scripts/Weapons/Hammer/HammerUpgrades/LightningBall.cs
@@ -1,30 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LightningBall", menuName = "Weapon Upgrades/Hammer Upgrades/LightningBall")]
 public class LightningBall : HammerUpgrade
 {
-    [SerializeField] GameObject lightningBallPrefab;
-    [SerializeField] float ballSpeed = 10f;
-    [SerializeField] float ballDamage = 5f;
-    [SerializeField] float ballSize = 1f;
-    [SerializeField] float ballLifetime = 3f;
+    [SerializeField] GameObject lightningBallPrefab; // should be a Ball prefab with BallController
+    [SerializeField] float minionSpeed = 6f;
+    [SerializeField] float minionContactDamage = 5f;
+    [SerializeField] float minionHealth = 1f;
+    [SerializeField] float minionSize = 1f;
 
     public override void OnBallHit(BallController otherBall)
     {
         base.OnBallHit(otherBall);
-        if (lightningBallPrefab == null || parentWeapon == null) return;
+        GameObject ball = Instantiate(lightningBallPrefab, otherBall.transform.position, Quaternion.identity);
+        ball.layer = parentBall.gameObject.layer + 4;
 
-        // choose a random direction (uniform around the circle)
-        Vector2 dir = Random.insideUnitCircle.normalized;
+        BallController bc = ball.GetComponent<BallController>();
+ 
+        bc.contactDamage = 5; // change ltr
+        bc.maxHealth = minionHealth;
+        bc.Init(new List<Upgrade>(), parentBall.playerNum, Random.Range(0.0f, 360.0f));
+        GameManager.Instance.GetMainBallByNumber(parentBall.playerNum).OnBallSpawned(bc);
 
-        GameObject ball = Instantiate(lightningBallPrefab, parentWeapon.transform.position, Quaternion.identity);
-        ball.layer = parentWeapon.gameObject.layer;
-        ball.transform.localScale = Vector3.one * ballSize;
-
-        Projectile proj = ball.GetComponent<Projectile>();
-        if (proj == null) { Destroy(ball); return; }
-
-        proj.lifetime = ballLifetime;
-        proj.ProjectileInit(dir, ballSpeed, ballDamage, parentWeapon);
     }
+    
 }
